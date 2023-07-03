ce2de7ffcb637d4f29bfd97606ce12a57f93aa4e
Arpita Panda <arpitapanda24088@gmail.com>
2023-07-03 23:12:20 +0530

    cover page modified

--- app.js
+++ app.js
@@ -108,5 +108,27 @@ showPokemonImage(currentIndex);
 // Add buttons to the slider
 slider.appendChild(prevBtn);
 slider.appendChild(nextBtn);
+let preveiwContainer = document.querySelector('.game-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.poke-container .game').forEach(game =>{
+  game.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = game.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
 
 
